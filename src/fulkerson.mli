0cf0f14c2fa0c204dd082531d02558e726caa618
Thibault-reb <trebeille@gmail.com>
2025-12-26 13:01:40 +0100

    Add residual_to_original function signature

--- src/fulkerson.mli
+++ src/fulkerson.mli
@@ -3,3 +3,4 @@ open Graph
 val min_capacity : int graph -> Path.path -> int
 val update_residual : int graph -> Path.path -> int -> int graph
 val ford_fulkerson : int graph -> id -> id -> int * int graph
+val residual_to_original : id graph -> id graph -> string graph
